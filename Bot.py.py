d690ea0ae2da4fcd69aa2d59073768ec1ff66da7
Luxs0 <loriforzanaapoli@gmail.com>
2023-09-27 17:48:23 +0200

    Ora riesce a mandare qualche messaggio

--- Bot.py.py
+++ Bot.py.py
@@ -1,22 +1,33 @@
 
-import discord
-from discord.ext import commands
+from discord import Client
+from requests import get
 
 # Configura il tuo bot
 TOKEN = 'MTE1NTk1NTk0OTc3Mzc5OTQ5NQ.GjRW6Q.ZH08rkshrR_pol-Vbq2iizu0xuWc1ydQdKHIAs'
 PREFIX = '!'
 
 # Crea un'istanza del bot
-bot = commands.Bot(command_prefix=PREFIX)
+CHANNEL_ID=1156612455963828245
 
-@bot.command(name='saluta', help='Il bot ti saluterà in un canale testuale.')
-async def saluta(ctx):
+bot = Client()
+
+@bot.event
+async def on_ready():
+    response = get("https://api.kucoin.com/api/v1/market/stats?symbol=BTC-EUR")
+    prezzo_attuale = float(response.json()['data']['buy'])
+    canale = bot.get_channel(CHANNEL_ID)
+    testo = f'Il prezzo attuale per il 1 BTC é pari a {prezzo_attuale} euro'
+    await canale.send(testo)
+
+@bot.event
+async def on_message(ctx):
     author = ctx.author
-    await ctx.send(f'Ciao, {author.mention}!')
+    testo = ctx.content
+    canale = ctx.channel
+    if author == bot.user:
+        return
+    await canale.send(f'Hai ragione, papera é proprio scemo, {author.mention}!')
 
-@bot.command(name='papera', help='Il bot risponderà con una risposta personalizzata.')
-async def papera(ctx):
-    await ctx.send(f'Che papera cornuta, {ctx.author.mention}!')
 
 # Avvia il bot
 bot.run(TOKEN)
