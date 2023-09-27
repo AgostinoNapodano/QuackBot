b36533efeaa734fa43f72242cae804b21b93b79b
Luxs0 <loriforzanaapoli@gmail.com>
2023-09-27 18:10:10 +0200

    Comandi implementati

--- Bot.py.py
+++ Bot.py.py
@@ -11,22 +11,29 @@ CHANNEL_ID=1156612455963828245
 
 bot = Client()
 
-@bot.event
-async def on_ready():
-    response = get("https://api.kucoin.com/api/v1/market/stats?symbol=BTC-EUR")
-    prezzo_attuale = float(response.json()['data']['buy'])
-    canale = bot.get_channel(CHANNEL_ID)
-    testo = f'Il prezzo attuale per il 1 BTC é pari a {prezzo_attuale} euro'
-    await canale.send(testo)
+#@bot.event
+#async def on_ready():
+#    response = get("https://api.kucoin.com/api/v1/market/stats?symbol=BTC-EUR")
+#    prezzo_attuale = float(response.json()['data']['buy'])
+#    canale = bot.get_channel(CHANNEL_ID)
+#    testo = f'Il prezzo attuale per il 1 BTC é pari a {prezzo_attuale} euro'
+#    await canale.send(testo)
 
 @bot.event
-async def on_message(ctx):
-    author = ctx.author
-    testo = ctx.content
-    canale = ctx.channel
+async def on_message(message):
+    author = message.author
+    testoMs = message.content
+    canale = message.channel
     if author == bot.user:
         return
-    await canale.send(f'Hai ragione, papera é proprio scemo, {author.mention}!')
+    if testoMs.startswith('!btc'):
+        response = get("https://api.kucoin.com/api/v1/market/stats?symbol=BTC-EUR")
+        prezzo_attuale = float(response.json()['data']['buy'])
+        canale = bot.get_channel(CHANNEL_ID)
+        testo = f'Il prezzo attuale per il 1 BTC é pari a {prezzo_attuale} euro'
+        await canale.send(testo)
+    if testoMs.startswith('!papera'):
+        await canale.send(f'Hai ragione, papera é proprio scemo, {author.mention}!')
 
 
 # Avvia il bot
